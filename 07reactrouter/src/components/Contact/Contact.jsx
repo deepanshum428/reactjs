18a3e11c6034a701f6f6cf1f2c342370e25e45a1
deepanshum428 <deepanshumalviya428@gmail.com>
2025-04-19 20:00:03 +0530

    remove all node module because of size

--- 07reactrouter/src/components/Contact/Contact.jsx
+++ 07reactrouter/src/components/Contact/Contact.jsx
@@ -3,20 +3,24 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <div className="contact-container">
-      <h1 className="heading">Contact us</h1>
-      <form action="">
-        <label htmlFor="name">Name</label>
-        <input type="text" placeholder="Name" />
+    <div className="main-div">
+      <div className="contact-container">
+        <h1 className="heading">Contact us</h1>
+        <form action="">
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="Enter name" />
 
-        <label htmlFor="email">Email</label>
-        <input type="email" placeholder="Email" />
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="Enter email" />
 
-        <label htmlFor="number">Contact number</label>
-        <input type="number" placeholder="Contact number" />
+          <label htmlFor="number">Contact number</label>
+          <input type="number" placeholder="Enter contact number" />
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit" onClick={alert("Submit successfull")}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
